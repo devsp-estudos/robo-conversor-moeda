9572a122aca6aaa178d799149f499923c64ef74c
DevSp <contatodevsp@gmail.com>
2020-06-14 17:59:11 -0300

    Pegando os inputs do usuario e mostrando o resultado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const puppeteer = require('puppeteer')
+const readlineSync = require('readline-sync')
 
 async function robo() {
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
 
-    const UrlGoogle = `https://www.google.com/search?q=dolar+para+real&oq=dolar+para+real&aqs=chrome..69i57j0l7.2760j0j7&sourceid=chrome&ie=UTF-8`
+    const moedaBase = readlineSync.question('Informe uma moeda base (dolar, euro...): ') || 'dolar'
+    const moedaFinal = readlineSync.question('Informe uma moeda desejada (real): ') || 'real'
+
+    const UrlGoogle = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&oq=${moedaBase}+para+${moedaFinal}&aqs=chrome..69i57j0l7.2760j0j7&sourceid=chrome&ie=UTF-8`
 
     await page.goto(UrlGoogle)
 
     const valorDoDolar = await page.evaluate(() => {
-        let resposta = document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc').value
+        let resposta = document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc')
 
-        if (resposta) resposta = Number(resposta)
+        if (resposta) resposta = Number(resposta.value)
 
         return resposta
     })
